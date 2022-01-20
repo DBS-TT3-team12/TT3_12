52ef3ef2053a28900ddf4b31cd632cee1cac9723
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-01-20 14:36:07 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
-import models
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask.json import jsonify, dumps
 
 from flask_jwt_extended import create_access_token, unset_jwt_cookies, jwt_required, JWTManager
+
+import models
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -14,10 +15,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Www.62511665@127.0.0.1/soc
 jwt = JWTManager(app)
 CORS(app)
 db = SQLAlchemy(app)
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    return {"msg": "PublicAccess"}
 
 @app.route('/login', methods=["POST"])
 def create_token():
@@ -38,16 +35,9 @@ def logout():
     unset_jwt_cookies(response)
     return response
 
-@app.route('/private', methods=["GET"])
-@jwt_required()
-def viewPrivate():
-    response = {
-        "msg": "AuthUser"
-    }
-    return response
-
 #This is to get all the posts
 @app.route('/allPost', methods = ["GET"])
+@jwt_required()
 def getAllPost():
     #Check if method signature is 'GET'
     if request.method == 'GET':
@@ -59,8 +49,9 @@ def getAllPost():
     return jsonify(result=result)
             
    
-#This is to get all the posts
+#This is to insert new post entry
 @app.route('/addPost', methods = ["POST"])
+@jwt_required()
 def addPost():
     #Check if method signature is 'POST'
     if request.method == 'POST':
@@ -71,13 +62,15 @@ def addPost():
         
         return {"status": "success",  "errorMsg": ""}
           
-#This is to get all the posts
+#This is to update post entry
 @app.route('/editPost', methods = ["POST"])
+@jwt_required()
 def editPost():
     #Check if method signature is 'POST'
     if request.method == 'POST':
         post = request.json
-        post_exists = models.Post.query.filter_by(Post_ID=post['postId'].first())
+        post_id = post['postId']
+        post_exists = models.Post.query.filter_by(Post_ID=post_id).first()
         
         if post_exists is None:
             return {"status": "fail",  "errorMsg": "Post Not existed"}
@@ -90,14 +83,15 @@ def editPost():
             
         return {"status": "success",  "errorMsg": ""}
         
-#This is to get all the posts
+#This is to remove a post entry
 @app.route('/deletePost', methods = ["POST"])
+@jwt_required()
 def deletePost():
     #Check if method signature is 'POST'
     if request.method == 'POST':
         post = request.json
-        
-        post_exists = models.Post.query.filter_by(Post_ID=post['postId'].first())
+        post_id = post['postId']
+        post_exists = models.Post.query.filter_by(Post_ID=post_id).first()
         
         if post_exists is None:
             return {"status": "fail",  "errorMsg": "Post Not existed"}
