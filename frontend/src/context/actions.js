c4e553113c9bb3c411b7395c18e700e7f02e3240
Shuming <geshuming2@gmail.com>
2022-01-20 15:27:54 +0800

    Add CRUD api and login api

--- frontend/src/context/actions.js
+++ frontend/src/context/actions.js
@@ -1,53 +1,51 @@
-import axios from "axios";
-
 const ROOT_URL = "http://127.0.0.1:5000";
 
 // working
-// export async function loginUser(dispatch, data) {
-//   const requestOptions = {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(data),
-//   };
+export async function loginUser(dispatch, data) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
 
-//   try {
-//     dispatch({ type: "REQUEST_LOGIN" });
-//     let response = await fetch(`${ROOT_URL}/login`, requestOptions);
-//     let res = await response.json();
-
-//     if (res.access_token) {
-//       const payload = { user: data.user, token: res.access_token };
-//       dispatch({
-//         type: "LOGIN_SUCCESS",
-//         payload,
-//       });
-//       localStorage.setItem("currentUser", JSON.stringify(payload));
-//       return payload;
-//     }
-
-//     dispatch({ type: "LOGIN_ERROR", error: res.msg });
-//     return;
-//   } catch (error) {
-//     dispatch({ type: "LOGIN_ERROR", error: error });
-//   }
-// }
-
-// frontend test
-export async function loginUser(dispatch, payload) {
   try {
     dispatch({ type: "REQUEST_LOGIN" });
+    let response = await fetch(`${ROOT_URL}/login`, requestOptions);
+    let res = await response.json();
 
-    if (payload.user === "admin" && payload.password === "password") {
-      dispatch({ type: "LOGIN_SUCCESS", payload: { user: payload.user, token: payload.password } });
-
-      return true;
+    if (res.access_token) {
+      const payload = { user: data.user, token: res.access_token };
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload,
+      });
+      localStorage.setItem("currentUser", JSON.stringify(payload));
+      return payload;
     }
-    dispatch({ type: "LOGIN_ERROR", error: "The username and password does not match." });
+
+    dispatch({ type: "LOGIN_ERROR", error: res.msg });
     return;
   } catch (error) {
-    // no need to check
+    dispatch({ type: "LOGIN_ERROR", error: error });
   }
 }
+
+// frontend test
+// export async function loginUser(dispatch, payload) {
+//   try {
+//     dispatch({ type: "REQUEST_LOGIN" });
+
+//     if (payload.user === "admin" && payload.password === "password") {
+//       dispatch({ type: "LOGIN_SUCCESS", payload: { user: payload.user, token: payload.password } });
+
+//       return true;
+//     }
+//     dispatch({ type: "LOGIN_ERROR", error: "The username and password does not match." });
+//     return;
+//   } catch (error) {
+//     // no need to check
+//   }
+// }
 
 export async function logout(dispatch) {
   dispatch({ type: "LOGOUT" });
