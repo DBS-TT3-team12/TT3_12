fbb965650ae5eac6ab09d175e7f2dbf2ad024e39
gargipandkar <54140892+gargipandkar@users.noreply.github.com>
2022-01-20 16:52:04 +0800

    Fix delete

--- backend/models.py
+++ backend/models.py
@@ -19,21 +19,21 @@ class Post(db.Model):
     Post_Title = db.Column(db.String(1000), nullable = False)
     Post_Description = db.Column(db.String(1000), nullable = False)
     Post_Image = db.Column(db.String(10000), nullable = False)
-    post_comment = db.relationship("POST_COMMENT", backref = "post")
-    liked_post = db.relationship("LIKED_POST", backref = "post")
+    post_comment = db.relationship("POST_COMMENT", cascade='all, delete-orphan', backref = "post")
+    liked_post = db.relationship("LIKED_POST", cascade='all, delete-orphan', backref = "post")
     User_ID = db.Column(db.Integer, nullable = False, default=1)
 
 # POST_COMMENT TABLE
 class POST_COMMENT(db.Model):
     Comment_ID = db.Column(db.Integer, primary_key = True)
-    Post_ID = db.Column(db.Integer, db.ForeignKey("post.Post_ID"))
+    Post_ID = db.Column(db.Integer, db.ForeignKey("post.Post_ID", ondelete='CASCADE'))
     User_ID = db.Column(db.Integer, db.ForeignKey("user.User_ID"))
     Comment = db.Column(db.String(10000), nullable = False)
 
 # LIKED_POST TABLE
 class LIKED_POST(db.Model):
     User_ID = db.Column(db.Integer, db.ForeignKey("user.User_ID"), primary_key = True)
-    Post_ID = db.Column(db.Integer, db.ForeignKey("post.Post_ID"), primary_key = True)
+    Post_ID = db.Column(db.Integer, db.ForeignKey("post.Post_ID", ondelete='CASCADE'), primary_key = True)
 
 # # Run this code once to create the database
 # db.create_all()
