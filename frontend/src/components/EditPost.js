cfac76583e3ceda3045faf245c50cdd362b0a9f8
Shuming <geshuming2@gmail.com>
2022-01-20 16:38:03 +0800

    Integrate editpost api

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import { Layout, Form, Button, Input } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useAuthState } from "../context/context";
+import { editPost } from "./api";
 
 const { Header, Content, Footer } = Layout;
 
-function EditPost({post}) {
-  console.log(post)
-  const fields = [post]
+function EditPost({ post }) {
+  // console.log(post)
+  const fields = [post];
+  const auth = useAuthState();
   const navigate = useNavigate();
   const onFinish = (values) => {
-    console.log('Success:', values)
-  }
+    editPost(auth.token, { ...values, postId: post.Post_ID }).then((result) => {
+      if (result) {
+        navigate("/");
+      } else {
+        console.error("Error: editPost");
+      }
+    });
+  };
 
   const handleBack = async (e) => {
     navigate("/");
-  }
+  };
 
   return (
     <Layout className="layout">
@@ -25,7 +34,7 @@ function EditPost({post}) {
         <h1>Tweeter</h1>
         <p> </p>
       </Header>
-      <Content style={{ padding: '50px' }}>
+      <Content style={{ padding: "50px" }}>
         <h2>Edit post:</h2>
         <Form
           name="basic"
@@ -34,21 +43,16 @@ function EditPost({post}) {
           initialValues={post}
           onFinish={onFinish}
         >
-          <Form.Item
-            name="title"
-            rules={[{ required: true, message: 'Please give a title!' }]}
-          >
+          <Form.Item name="title" rules={[{ required: true, message: "Please give a title!" }]}>
             <Input placeholder="Title" />
           </Form.Item>
           <Form.Item
             name="desc"
-            rules={[{ required: true, message: 'Please write a short description!' }]}
+            rules={[{ required: true, message: "Please write a short description!" }]}
           >
             <Input.TextArea rows={4} placeholder="Write a caption..." />
           </Form.Item>
-          <Form.Item
-            name="image"
-          >
+          <Form.Item name="image">
             <Input placeholder="image hyperlink" />
           </Form.Item>
           <Form.Item>
